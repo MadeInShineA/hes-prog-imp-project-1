fcadaa2c85bfee57f3ab57b53ba6087610c624cf
MadeInShineA <olivier.amacker@netplus.ch>
2023-12-18 11:40:44 +0100

    Updated the code to say Hello to the user once it's connected

--- Client.scala
+++ Client.scala
@@ -1,16 +1,23 @@
 package Project1
 
+import hevs.graphics.FunGraphics
+
 import java.io.PrintStream
 import java.net.{InetAddress, Socket}
 import scala.io.BufferedSource
 
 object Client extends App{
-  val server = new Socket(InetAddress.getByName("localhost"), 666)
+  val server = new Socket(InetAddress.getByName("localhost"), 1)
   val in = new BufferedSource(server.getInputStream).getLines()
   val out = new PrintStream(server.getOutputStream)
+  var counter: Int = 0
 
   out.println("Hello, Server!")
-  println("Server says: " + in.next())
+  while (true){
+    if(in.hasNext){
+      println("Server says: " + in.next())
+    }
+  }
 
   server.close()
 
--- Server.scala
+++ Server.scala
@@ -1,13 +1,21 @@
 package Project1
 
-import java.net.ServerSocket
+import java.io.PrintStream
+import java.net.{ServerSocket, Socket}
+import scala.collection.mutable.ArrayBuffer
 
 object Server extends App{
-  val server: ServerSocket = new ServerSocket(666)
+  val server: ServerSocket = new ServerSocket(1)
   println("Server started, waiting for connections ")
 
+  val clients: ArrayBuffer[Socket] = new ArrayBuffer()
+
   while(true){
-    val client = server.accept()
+    val client: Socket = server.accept()
+    clients.addOne(client)
+    val out = new PrintStream(client.getOutputStream)
+    out.println("Hello user")
+
     val clientHandler: ClientHandler = new ClientHandler(client)
     new Thread(clientHandler).start()
   }
